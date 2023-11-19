4e689131248ce07249e1a54e56475ec6bfe4c3fa
Gerald Saberon <79496236+geraldsaberon@users.noreply.github.com>
2023-11-19 20:33:05 +0800

    Refactor POSTER_DIR config

--- letterboxd_scraper/config.py
+++ letterboxd_scraper/config.py
@@ -2,13 +2,10 @@ import os
 from pathlib import Path
 from dataclasses import dataclass
 
-POSTER_DIR = Path(".").absolute() / "posters"
-try:
-    os.mkdir(Path(__file__).parent.parent.resolve() / "posters")
-    POSTER_DIR = Path(__file__).parent.parent.resolve() / "posters"
-except FileExistsError:
-    """posters directory already exists"""
-    pass
+
+POSTER_DIR = Path(__file__).parent.parent.resolve() / "posters"
+if not POSTER_DIR.is_dir():
+    os.mkdir(POSTER_DIR)
 
 @dataclass
 class IMG_DIM:
