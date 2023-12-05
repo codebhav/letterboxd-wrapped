8a6b1b6539c24d8d363e0546a0ce4ebee680456a
Gerald Saberon <79496236+geraldsaberon@users.noreply.github.com>
2023-12-06 02:44:34 +0800

    Fix error when --size is not provided

--- letterboxdcollage.py
+++ letterboxdcollage.py
@@ -10,22 +10,19 @@ if __name__ == "__main__":
         help="Letterboxd username"
     )
     parser.add_argument(
-        "--size",
-        "-s",
+        "--size", "-s",
         help="Collage size (WIDTH, HEIGHT). WIDTH * HEIGHT must be 50 or less",
         nargs=2,
         type=int,
     )
     args = parser.parse_args()
-    WIDTH, HEIGHT = args.size
-    collage_size = (5, 5)
+    WIDTH, HEIGHT = (5, 5)
     if args.size:
+        WIDTH, HEIGHT = args.size
         if WIDTH * HEIGHT > 50:
             print("Invalid arguments to --size WIDTH HEIGHT")
             print("WIDTH * HEIGHT must be 50 or less")
             exit()
-        else:
-            collage_size = (WIDTH, HEIGHT)
     Collage(LetterboxdUser(args.username)) \
-        .create(size=collage_size) \
+        .create(size=(WIDTH, HEIGHT)) \
         .save(f"{args.username}_collage_{WIDTH}x{HEIGHT}.jpg")
